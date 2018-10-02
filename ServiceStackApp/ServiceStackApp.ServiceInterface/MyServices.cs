ebd939035509eafb539689c6a5ec2d3157164592
bm295 <baominh.nguyen.295@gmail.com>
2018-10-02 23:12:37 +0700

    init project

---
+++ ServiceStackApp/ServiceStackApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using ServiceStackApp.ServiceModel;
+
+namespace ServiceStackApp.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
